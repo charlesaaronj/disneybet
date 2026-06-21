67efbf57051b8ac7ea1aa77776f171fb620c92b8
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-06-21 15:19:37 -0500

    Update data-HollywoodStudios.js

--- data-HollywoodStudios.js
+++ data-HollywoodStudios.js
@@ -2,28 +2,28 @@ const PARK_HOLLYWOOD_STUDIOS = {
   name: "Hollywood Studios",
   attractions: [
     // Hollywood Boulevard
-    { land: "Hollywood Boulevard",   name: "Runaway Railway" },
+    { park: "Hollywood Studios", land: "Hollywood Boulevard",   name: "Runaway Railway" },
 
     // Sunset Boulevard
-    { land: "Sunset Boulevard",      name: "Tower of Terror" },
-    { land: "Sunset Boulevard",      name: "Rock 'n' Roller Coaster" },
+    { park: "Hollywood Studios", land: "Sunset Boulevard",      name: "Tower of Terror" },
+    { park: "Hollywood Studios", land: "Sunset Boulevard",      name: "Rock 'n' Roller Coaster" },
 
     // Echo Lake
-    { land: "Echo Lake",             name: "Star Tours" },
-    { land: "Echo Lake",             name: "Indiana Jones Stunt Spectacular" },
-    { land: "Echo Lake",             name: "Frozen Sing-Along" },
+    { park: "Hollywood Studios", land: "Echo Lake",             name: "Star Tours" },
+    { park: "Hollywood Studios", land: "Echo Lake",             name: "Indiana Jones Stunt Spectacular" },
+    { park: "Hollywood Studios", land: "Echo Lake",             name: "Frozen Sing-Along" },
 
     // Grand Avenue
-    { land: "Grand Avenue",          name: "Monsters Inc. Door Coaster" },
+    { park: "Hollywood Studios", land: "Grand Avenue",          name: "Monsters Inc. Door Coaster" },
 
     // Toy Story Land
-    { land: "Toy Story Land",        name: "Slinky Dog Dash" },
-    { land: "Toy Story Land",        name: "Toy Story Mania" },
-    { land: "Toy Story Land",        name: "Alien Swirling Saucers" },
+    { park: "Hollywood Studios", land: "Toy Story Land",        name: "Slinky Dog Dash" },
+    { park: "Hollywood Studios", land: "Toy Story Land",        name: "Toy Story Mania" },
+    { park: "Hollywood Studios", land: "Toy Story Land",        name: "Alien Swirling Saucers" },
 
     // Galaxy's Edge
-    { land: "Galaxy's Edge",         name: "Millennium Falcon: Smugglers Run" },
-    { land: "Galaxy's Edge",         name: "Rise of the Resistance" },
+    { park: "Hollywood Studios", land: "Galaxy's Edge",         name: "Millennium Falcon: Smugglers Run" },
+    { park: "Hollywood Studios", land: "Galaxy's Edge",         name: "Rise of the Resistance" },
   ],
   genericQuestions: [
     // ... (same as other parks)
