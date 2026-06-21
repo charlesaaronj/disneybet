34580aa3d6e7a8a36c17863358782ff10b810ed3
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-06-21 15:18:58 -0500

    Update data-epcot.js

--- data-epcot.js
+++ data-epcot.js
@@ -2,30 +2,30 @@ const PARK_EPCOT = {
   name: "EPCOT",
   attractions: [
     // World Celebration
-    { land: "World Celebration", name: "Spaceship Earth" },
-    { land: "World Celebration", name: "Figment" },
-    { land: "World Celebration", name: "Short Film Festival" },
+    { park: "EPCOT", land: "World Celebration", name: "Spaceship Earth" },
+    { park: "EPCOT", land: "World Celebration", name: "Figment" },
+    { park: "EPCOT", land: "World Celebration", name: "Short Film Festival" },
 
     // World Discovery
-    { land: "World Discovery", name: "Guardians: Cosmic Rewind" },
-    { land: "World Discovery", name: "Mission: SPACE" },
-    { land: "World Discovery", name: "Test Track" },
+    { park: "EPCOT", land: "World Discovery", name: "Guardians: Cosmic Rewind" },
+    { park: "EPCOT", land: "World Discovery", name: "Mission: SPACE" },
+    { park: "EPCOT", land: "World Discovery", name: "Test Track" },
 
     // World Nature
-    { land: "World Nature", name: "Soarin'" },
-    { land: "World Nature", name: "Living with the Land" },
-    { land: "World Nature", name: "Journey of Water" },
-    { land: "World Nature", name: "The Seas w/ Nemo & Friends" },
-    { land: "World Nature", name: "Turtle Talk with Crush" },
+    { park: "EPCOT", land: "World Nature", name: "Soarin'" },
+    { park: "EPCOT", land: "World Nature", name: "Living with the Land" },
+    { park: "EPCOT", land: "World Nature", name: "Journey of Water" },
+    { park: "EPCOT", land: "World Nature", name: "The Seas w/ Nemo & Friends" },
+    { park: "EPCOT", land: "World Nature", name: "Turtle Talk with Crush" },
 
     // World Showcase
-    { land: "Mexico",           name: "Gran Fiesta Tour" },
-    { land: "Norway",           name: "Frozen Ever After" },
-    { land: "China",            name: "Reflections of China" },
-    { land: "America",          name: "The American Adventure" },
-    { land: "France",           name: "Remy's Ratatouille Adventure" },
-    { land: "France",           name: "Beauty & the Beast Sing-Along" },
-    { land: "Canada",           name: "Canada Far and Wide" },
+    { park: "EPCOT", land: "Mexico",  name: "Gran Fiesta Tour" },
+    { park: "EPCOT", land: "Norway",  name: "Frozen Ever After" },
+    { park: "EPCOT", land: "China",   name: "Reflections of China" },
+    { park: "EPCOT", land: "America", name: "The American Adventure" },
+    { park: "EPCOT", land: "France",  name: "Remy's Ratatouille Adventure" },
+    { park: "EPCOT", land: "France",  name: "Beauty & the Beast Sing-Along" },
+    { park: "EPCOT", land: "Canada",  name: "Canada Far and Wide" },
   ],
   genericQuestions: [
     // ... (same as Magic Kingdom)
