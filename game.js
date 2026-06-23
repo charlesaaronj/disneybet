f3c6fb0e72445406718d9da71554f582e6f793d9
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-06-23 08:55:37 -0500

    Update game.js

--- game.js
+++ game.js
@@ -405,15 +405,10 @@ function initHeroSpotlightAfterWelcome() {
   }
 
 // Called once from splash after first-time onboarding completes
-function initHeroSpotlightFirstVisit() {
-  console.log("[spotlight] first-visit init");
-
-  // Use whatever screen is currently stored, default to setup-game
-  var screenName = (window.gameState && window.gameState.screen) || "setup-game";
-
-  // Only show once per screen; reuse existing helper
-  showHeroSpotlightForScreen(screenName);
-}
+window.initHeroSpotlightFirstVisit = function () {
+  console.log("[spotlight] first-visit init for setup-game");
+  showHeroSpotlightForScreen("setup-game");
+};
 
   modalEl.addEventListener(
     "hidden.bs.modal",
