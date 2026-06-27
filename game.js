da9106a4da1c579cd6f85b35cd4db9029e5db00e
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-06-27 04:37:58 -0500

    Update game.js

--- game.js
+++ game.js
@@ -537,7 +537,7 @@ function setQuestionDisplay(text) {
   if (display) {
     const isPrompt =
       !text ||
-      text.trim() === "Select the attraction you're queued for.";
+      text.trim() === "Select the attraction you're in line for anove 👆.";
 
     display.style.display = text ? "block" : "none";
     display.textContent = text || "";
