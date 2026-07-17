65e369d0250bd6f41cc5e6ac1d537f6554aa52e1
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-07-16 22:57:37 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3056,7 +3056,7 @@ document.getElementById('wsd-gw-guess')?.addEventListener('change', () => {
     if (authorNote) {
       console.log('Showing authorNote for author');
       authorNote.textContent =
-        "You wrote this answer. You may choose a guess, but you cannot wager. Your wager is always 0 and does not affect scores.";
+        "You wrote this answer so your wager is automatically 0.";
       authorNote.style.display = 'block';
     }
 
