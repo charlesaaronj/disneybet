2ce62a02d6c423a21ef98dff69bea3ca2a982891
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-07-23 09:20:09 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 //   Who Said Diz —  game.js (refactored, commented, same behavior)
 // ===========================================================
 
-const APP_VERSION = "1.5";
+const APP_VERSION = "1.6";
 
 // ---------- Small DOM + utility helpers ----------
 const $ = id => document.getElementById(id);
