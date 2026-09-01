646de3da6afe2aaf67a138d69c98e91a47be5971
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-09-01 16:20:40 -0500

    Update data-mk.js

--- data-mk.js
+++ data-mk.js
@@ -1,36 +1,222 @@
 const PARK_MAGIC_KINGDOM = {
   name: "Magic Kingdom",
   attractions: [
-    { park: "Magic Kingdom", land: "Main Street, U.S.A.",  name: "Railroad" },
-    { park: "Magic Kingdom", land: "Adventureland",        name: "Pirates" },
-    { park: "Magic Kingdom", land: "Adventureland",        name: "Jungle Cruise" },
-    { park: "Magic Kingdom", land: "Adventureland",        name: "Magic Carpets" },
-    { park: "Magic Kingdom", land: "Adventureland",        name: "Tiki Room" },
-    { park: "Magic Kingdom", land: "Adventureland",        name: "Swiss Treehouse" },
-    { park: "Magic Kingdom", land: "Frontierland",         name: "Big Thunder" },
-    { park: "Magic Kingdom", land: "Frontierland",         name: "Tiana's Bayou" },
-    { park: "Magic Kingdom", land: "Frontierland",         name: "Country Bears" },
-    { park: "Magic Kingdom", land: "Liberty Square",       name: "Haunted Mansion" },
-    { park: "Magic Kingdom", land: "Liberty Square",       name: "Hall of Presidents" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Small World" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Peter Pan" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Carrousel" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "PhilharMagic" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Winnie the Pooh" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Mad Tea Party" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Seven Dwarfs Mine Train" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Little Mermaid" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Enchanted Tales w/ Belle" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Dumbo" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Barnstormer" },
-    { park: "Magic Kingdom", land: "Fantasyland",          name: "Casey Jr. Splash" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "Astro Orbiter" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "Buzz Lightyear" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "PeopleMover" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "Space Mountain" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "Speedway" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "TRON" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "Carousel of Progress" },
-    { park: "Magic Kingdom", land: "Tomorrowland",         name: "Laugh Floor" },
+    {
+      park: "Magic Kingdom",
+      land: "Main Street, U.S.A.",
+      name: "Railroad",
+      type: "train-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Adventureland",
+      name: "Pirates",
+      type: "dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Adventureland",
+      name: "Jungle Cruise",
+      type: "boat-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Adventureland",
+      name: "Magic Carpets",
+      type: "spinner-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Adventureland",
+      name: "Tiki Room",
+      type: "show",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Adventureland",
+      name: "Swiss Treehouse",
+      type: "walkthrough",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Frontierland",
+      name: "Big Thunder",
+      type: "coaster",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Frontierland",
+      name: "Tiana's Bayou",
+      type: "log-flume",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Frontierland",
+      name: "Country Bears",
+      type: "show",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Liberty Square",
+      name: "Haunted Mansion",
+      type: "dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Liberty Square",
+      name: "Hall of Presidents",
+      type: "show",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Small World",
+      type: "dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Peter Pan",
+      type: "dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Carrousel",
+      type: "carousel",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "PhilharMagic",
+      type: "3d-show",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Winnie the Pooh",
+      type: "dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Mad Tea Party",
+      type: "spinner-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Seven Dwarfs Mine Train",
+      type: "coaster",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Little Mermaid",
+      type: "dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Enchanted Tales w/ Belle",
+      type: "interactive-show",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Dumbo",
+      type: "spinner-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Barnstormer",
+      type: "coaster",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Fantasyland",
+      name: "Casey Jr. Splash",
+      type: "splash-pad",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "Astro Orbiter",
+      type: "spinner-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "Buzz Lightyear",
+      type: "interactive-dark-ride",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "PeopleMover",
+      type: "transit-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "Space Mountain",
+      type: "coaster",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "Speedway",
+      type: "autopia-ride",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "TRON",
+      type: "coaster",
+      animatronics: false
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "Carousel of Progress",
+      type: "show",
+      animatronics: true
+    },
+    {
+      park: "Magic Kingdom",
+      land: "Tomorrowland",
+      name: "Laugh Floor",
+      type: "interactive-show",
+      animatronics: false
+    }
   ]
 };
