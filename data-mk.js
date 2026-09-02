1de8fb4bdc699222f5fbcfd4dc33252bed3aa422
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-09-02 09:55:58 -0500

    Update data-mk.js

--- data-mk.js
+++ data-mk.js
@@ -1,222 +1,295 @@
 const PARK_MAGIC_KINGDOM = {
   name: "Magic Kingdom",
   attractions: [
+    // Main Street, U.S.A.
     {
       park: "Magic Kingdom",
       land: "Main Street, U.S.A.",
       name: "Railroad",
-      type: "train-ride",
-      animatronics: false
+      type: "thrill-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
+
+    // Adventureland
     {
       park: "Magic Kingdom",
       land: "Adventureland",
       name: "Pirates",
-      type: "dark-ride",
-      animatronics: true
+      type: "boat-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Adventureland",
       name: "Jungle Cruise",
       type: "boat-ride",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Adventureland",
       name: "Magic Carpets",
       type: "spinner-ride",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Adventureland",
       name: "Tiki Room",
       type: "show",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Adventureland",
       name: "Swiss Treehouse",
       type: "walkthrough",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
+
+    // Frontierland
     {
       park: "Magic Kingdom",
       land: "Frontierland",
       name: "Big Thunder",
       type: "coaster",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Frontierland",
       name: "Tiana's Bayou",
-      type: "log-flume",
-      animatronics: true
+      type: "boat-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Frontierland",
       name: "Country Bears",
       type: "show",
-      animatronics: true
+      animatronics: true,
+      preshow: true,
+      postShow: false
     },
+
+    // Liberty Square
     {
       park: "Magic Kingdom",
       land: "Liberty Square",
       name: "Haunted Mansion",
       type: "dark-ride",
-      animatronics: true
+      animatronics: true,
+      preshow: true,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Liberty Square",
       name: "Hall of Presidents",
       type: "show",
-      animatronics: true
+      animatronics: true,
+      preshow: true,
+      postShow: false
     },
+
+    // Fantasyland
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Small World",
-      type: "dark-ride",
-      animatronics: true
+      type: "boat-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Peter Pan",
       type: "dark-ride",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Carrousel",
-      type: "carousel",
-      animatronics: false
+      type: "spinner-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "PhilharMagic",
-      type: "3d-show",
-      animatronics: false
+      type: "film",
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Winnie the Pooh",
       type: "dark-ride",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Mad Tea Party",
       type: "spinner-ride",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Seven Dwarfs Mine Train",
       type: "coaster",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Little Mermaid",
       type: "dark-ride",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Enchanted Tales w/ Belle",
-      type: "interactive-show",
-      animatronics: true
+      type: "show",
+      animatronics: true,
+      preshow: true,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Dumbo",
       type: "spinner-ride",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Barnstormer",
       type: "coaster",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Fantasyland",
       name: "Casey Jr. Splash",
-      type: "splash-pad",
-      animatronics: false
+      type: "play-area",
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
+
+    // Tomorrowland
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "Astro Orbiter",
       type: "spinner-ride",
-      animatronics: false
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "Buzz Lightyear",
-      type: "interactive-dark-ride",
-      animatronics: true
+      type: "dark-ride",
+      animatronics: true,
+      preshow: true,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "PeopleMover",
-      type: "transit-ride",
-      animatronics: false
+      type: "thrill-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "Space Mountain",
       type: "coaster",
-      animatronics: false
+      animatronics: false,
+      preshow: true,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "Speedway",
-      type: "autopia-ride",
-      animatronics: false
+      type: "thrill-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "TRON",
       type: "coaster",
-      animatronics: false
+      animatronics: false,
+      preshow: true,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "Carousel of Progress",
       type: "show",
-      animatronics: true
+      animatronics: true,
+      preshow: false,
+      postShow: false
     },
     {
       park: "Magic Kingdom",
       land: "Tomorrowland",
       name: "Laugh Floor",
-      type: "interactive-show",
-      animatronics: false
+      type: "show",
+      animatronics: false,
+      preshow: true,
+      postShow: false
     }
   ]
 };
