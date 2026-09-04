574ec23dcb809436b4c5d71b2e90d9d50ccde73b
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-09-04 07:22:27 -0500

    Update data-epcot.js

--- data-epcot.js
+++ data-epcot.js
@@ -2,32 +2,185 @@ const PARK_EPCOT = {
   name: "EPCOT",
   attractions: [
     // World Celebration
-    { park: "EPCOT", land: "World Celebration", name: "Spaceship Earth" },
-    { park: "EPCOT", land: "World Celebration", name: "Figment" },
-    { park: "EPCOT", land: "World Celebration", name: "Pixar Film Festival" },
+    {
+      park: "EPCOT",
+      land: "World Celebration",
+      name: "Spaceship Earth",
+      type: "dark-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: true
+    },
+    {
+      park: "EPCOT",
+      land: "World Celebration",
+      name: "Figment",
+      type: "dark-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "World Celebration",
+      name: "Pixar Film Festival",
+      type: "film",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
 
     // World Discovery
-    { park: "EPCOT", land: "World Discovery", name: "Guardians Cosmic Rewind" },
-    { park: "EPCOT", land: "World Discovery", name: "Mission: SPACE" },
-    { park: "EPCOT", land: "World Discovery", name: "Test Track" },
+    {
+      park: "EPCOT",
+      land: "World Discovery",
+      name: "Guardians Cosmic Rewind",
+      type: "coaster",
+      animatronics: false,
+      preshow: true,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "World Discovery",
+      name: "Mission: SPACE",
+      type: "simulator",
+      animatronics: false,
+      preshow: true,
+      postShow: true
+    },
+    {
+      park: "EPCOT",
+      land: "World Discovery",
+      name: "Test Track",
+      type: "thrill-ride",
+      animatronics: false,
+      preshow: true,
+      postShow: false
+    },
 
     // World Nature
-    { park: "EPCOT", land: "World Nature", name: "Soarin'" },
-    { park: "EPCOT", land: "World Nature", name: "Living with the Land" },
-    { park: "EPCOT", land: "World Nature", name: "Journey of Water" },
-    { park: "EPCOT", land: "World Nature", name: "The Seas w/ Nemo" },
-    { park: "EPCOT", land: "World Nature", name: "Turtle Talk" },
-    { park: "EPCOT", land: "World Nature", name: "Awesome Planet" },
+    {
+      park: "EPCOT",
+      land: "World Nature",
+      name: "Soarin'",
+      type: "simulator",
+      animatronics: false,
+      preshow: true,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "World Nature",
+      name: "Living with the Land",
+      type: "boat-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "World Nature",
+      name: "Journey of Water",
+      type: "walkthrough",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "World Nature",
+      name: "The Seas w/ Nemo",
+      type: "dark-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: true
+    },
+    {
+      park: "EPCOT",
+      land: "World Nature",
+      name: "Turtle Talk",
+      type: "show",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "World Nature",
+      name: "Awesome Planet",
+      type: "film",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
 
     // World Showcase
-    { park: "EPCOT", land: "Mexico",  name: "Gran Fiesta Tour" },
-    { park: "EPCOT", land: "Norway",  name: "Frozen Ever After" },
-    { park: "EPCOT", land: "China",   name: "Reflections of China" },
-    { park: "EPCOT", land: "America", name: "American Adventure" },
-    { park: "EPCOT", land: "France",  name: "Remy" },
-    { park: "EPCOT", land: "France",  name: "Beauty & Beast Sing-Along" },
-    { park: "EPCOT", land: "Canada",  name: "Canada Far and Wide" },
+    {
+      park: "EPCOT",
+      land: "Mexico",
+      name: "Gran Fiesta Tour",
+      type: "boat-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "Norway",
+      name: "Frozen Ever After",
+      type: "boat-ride",
+      animatronics: true,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "China",
+      name: "Reflections of China",
+      type: "film",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "America",
+      name: "American Adventure",
+      type: "show",
+      animatronics: true,
+      preshow: true,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "France",
+      name: "Remy",
+      type: "dark-ride",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "France",
+      name: "Beauty & Beast Sing-Along",
+      type: "film",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    },
+    {
+      park: "EPCOT",
+      land: "Canada",
+      name: "Canada Far and Wide",
+      type: "film",
+      animatronics: false,
+      preshow: false,
+      postShow: false
+    }
   ],
+
   genericQuestions: [
     // ... (same as Magic Kingdom)
   ]
