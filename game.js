cb0d23a513fbac22021897edcfaf8684d623511d
charlesaaronj <43152585+charlesaaronj@users.noreply.github.com>
2026-09-11 14:56:16 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2292,7 +2292,7 @@ function buildRevealSummaryLines(r) {
   } else if (ctx.authorWonRound) {
     line1 = `🎯 Nobody guessed the author. ${ctx.author.name} won the round.`;
   } else {
-    line1 = `🎉 ${ctx.winnerNames} guessed the authorities ${ctx.author.name}.`;
+    line1 = `🎉 ${ctx.winnerNames} guessed the author ${ctx.author.name}.`;
   }
 
   return [line1, line4, line2, line3].filter(Boolean);
